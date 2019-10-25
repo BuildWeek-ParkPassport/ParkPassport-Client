3af0676297a7f89941608958834a9c2cffb4be94
Kyle Corbin <kylecorbin26@gmail.com>
2019-10-25 12:12:28 -0400

    added now json file

--- park-passport/src/components/Navbar.js
+++ park-passport/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import logo from '../image/logo.png'
+import logo from '../image/park-finder-logo-name.png'
 import {
     Collapse,
     Navbar,
